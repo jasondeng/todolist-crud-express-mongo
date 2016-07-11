e93e541cbf6c8f7f0a6ec6ac9f1680cb41997a89
Jason Deng <jdeng1234@gmail.com>
2016-07-11 19:56:36 -0400

    initialized db and created form

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 
-app.listen(3000, function () {
-    console.log('Server listening on port 3000');
+app.use(bodyParser.urlencoded({ extended:true }));
+
+var config = require('./env.json');
+
+MongoClient.connect(config.MONGO_URI, (err, db) => {
+    if(err) return console.log(err);
+
+    app.listen(3000, () => {
+        console.log('Server listening on port 3000');
+    })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
 })
